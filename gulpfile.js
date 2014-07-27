7f84f5b5a374f6f0e8f2c79547f5441eb6412875
sole <listas@soledadpenades.com>
2014-07-28 00:46:40 +0100

    copypasta + modifyasta

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'); 
+
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var htmlreplace = require('gulp-html-replace');
+var minifycss = require('gulp-minify-css');
+
+gulp.task('lint', function() {
+	return gulp.src('src/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('data', function() {
+	return gulp.src(['src/data/**'])
+		.pipe(gulp.dest('dist/data'));
+});
+
+// Build minified production version including code for analytics
+gulp.task('app-min', function() {
+	return gulp.src([
+			'src/production/ga.js',
+			'src/main.js'
+		])
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('css-min', function() {
+	return gulp.src('src/style.css')
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist'));
+});
+
+// The production html file uses only one script file
+gulp.task('html-min', function() {
+	return gulp.src('src/index.html')
+		.pipe(htmlreplace({
+			'app': {
+				src: 'app.min.js',
+				tpl: '<script src="%s" defer></script>'
+			}
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/**/*.js', ['lint', 'app-min']);
+	gulp.watch('src/index.html', ['html-min']);
+});
+
+gulp.task('default', ['lint', 'data', 'app-min', 'css-min', 'html-min', 'watch']);
